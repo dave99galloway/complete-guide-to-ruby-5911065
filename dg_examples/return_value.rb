27137276b7e8ad17a536bfc794f946ed6a0172a0
dave99galloway <60669660+dave99galloway@users.noreply.github.com>
2026-01-14 14:33:47 +0000

    add hash and array return values

--- dg_examples/return_value.rb
+++ dg_examples/return_value.rb
@@ -5,3 +5,17 @@ def subtract(n1,n2)
 end
 
 puts subtract(8,3)
+
+def add_and_subtract_hash(n1,n2)
+  {:add => n1+n2, :sub => n1-n2}
+end
+
+def add_and_subtract(n1,n2)
+  [n1+n2, n1-n2]
+end
+
+result = add_and_subtract_hash(8,3)
+puts result
+a,b = add_and_subtract(3,8)
+puts a
+puts b
